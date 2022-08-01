93e28cf3cf04d6ff269753a14b7b849bd5c1167e
Donald Campbell <donaldcroxis@hotmail.com>
2022-07-31 21:04:21 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto/Proyecto/Views/SignUp.xaml.cs
@@ -0,0 +1,21 @@
+﻿using NPOI.POIFS.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Proyecto.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUp : ContentPage
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
